b4ae860af1140faaafa6d1a4d78bdca9b56e1706
Naomi Johnson <snjohnson789@gmail.com>
2024-11-19 13:36:53 -0800

    adding more context via prints

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import time
 from flask import Flask, session
-
-from projectsecrets import app_secret # Secret string for "signing" session data, stored separately similar to API keys
+# Secret string for "signing" session data, stored separately
+from projectsecrets import app_secret
 
 app = Flask(__name__)
 app.secret_key = app_secret
 
-
 @app.route("/")
 def index():
-   session["firstvisit"] = time.ctime()
+   login_time = time.ctime()
+   session["firstvisit"] = login_time
+   print(f"Another user just logged in at `{login_time}`!! Here's their key to their session: `{app_secret}`")
    return "<a href='/otherpage'>Click me</a> to go to the other page"
 
 
 @app.route("/otherpage")
 def otherpage():
    return "You started visiting this site at " + session["firstvisit"] 
+
+
+
